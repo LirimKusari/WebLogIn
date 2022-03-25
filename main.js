0671b97cdf61f68c26ac4c085374ff5fbd348ba5
Lirim Kusari <kusarilirim@gmail.com>
2022-03-25 14:42:32 +0100

    html and css is done, started validiation

---
+++ main.js
@@ -0,0 +1,16 @@
+let first = document.querySelector("#First_Name");
+let last = document.querySelector("#Last_Name");
+let username = document.querySelector("Username");
+let email = document.querySelector("Email");
+let phone = document.querySelector("Phone");
+let birth_date = document.querySelector("Birth_Date");
+let psw = document.querySelector("password");
+let psw2 = document.querySelector("password2");
+
+// let a = document.querySelectorAll("input");
+// a.forEach(a => a.addEventListener("keyup",func )
+
+first.addEventListener("keyup", (e) => {
+  if (first.value.length < 5) {
+  }
+});
